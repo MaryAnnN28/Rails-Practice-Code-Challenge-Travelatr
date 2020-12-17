92e6dec0b964c8c548ec94842139145096750215
MaryAnnN28 <maryann.navarrete@gmail.com>
2020-12-16 19:42:06 -0500

    Done. (Except bonus on blogger)

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,36 @@
 class Blogger < ApplicationRecord
+     has_many :posts
+     has_many :destinations, through: :posts
+
+     validates :name, presence: true 
+     validates :name, uniqueness: true 
+     validates :age, presence: true
+     validates :age, numericality: { greater_than: 0 }
+     # validates :bio, presence: true
+     # validates :bio, length: { minimum: 30 }
+
+     def total_likes
+          total = self.posts.map do |post|
+               post.likes
+          end
+          total.sum
+     end
+
+     def featured_post
+          self.posts.order(likes: :desc)[0]
+     end
+
+     def top_5_destinations
+          destinations = self.posts.group_by do |post|
+               post.destination
+          end
+          sorted = destinations.sort_by do |dest, posts|
+               posts.length
+          end
+          array = sorted.map do |dest|
+               dest[0]
+          end
+          return array[0..4]
+     end
 
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,32 @@
 class Destination < ApplicationRecord
+     has_many :posts
+     has_many :bloggers, through: :posts
 
+     def featured_post
+          self.posts.order(likes: :desc)[0]
+     end
+
+
+     
+     def most_recent_posts
+          self.posts.order("created_at desc").limit(5)
+     end 
+
+
+
+     def average_age
+          if self.bloggers.length < 1
+               return "There are no posts about this destination yet."
+          end
+          unique_bloggers = self.bloggers.distinct
+          total_age = 0 
+          unique_bloggers.each do |blogger|
+               total_age += blogger.age 
+          end
+          average_age = total_age/unique_bloggers.length
+          return average_age
+     end
+
+
+ 
 end
